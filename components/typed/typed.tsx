3ae333133aed902e86334b4e00559ea65e771289
commandx1 <serhatbelen7@gmail.com>
2025-04-08 17:45:24 +0300

    fix error msg simulation

--- components/typed/typed.tsx
+++ components/typed/typed.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Roboto_Mono } from 'next/font/google';
+import { useEffect, useRef } from 'react';
 import { ReactTyped } from 'react-typed';
 
 import styles from './typed.module.scss';
@@ -11,8 +12,36 @@ const fontFamily = Roboto_Mono({
     subsets: ['latin'],
 });
 
+const err1 = 'Type "number" is not assignable to type "string"';
+const err2 = 'Type "string" is not assignable to type "string[]"';
+
 const Typed = () => {
+    const err1Ref = useRef<HTMLDivElement>(null);
+    const err2Ref = useRef<HTMLDivElement>(null);
+
     const { strings } = useText();
+
+    const timeout = (delay: number) => {
+        return new Promise(resolve => setTimeout(resolve, delay));
+    };
+
+    useEffect(() => {
+        const showErrorMessages = async () => {
+            await timeout(3800);
+            err1Ref.current!.style.display = 'block';
+
+            await timeout(3100);
+            err1Ref.current!.style.display = 'none';
+
+            await timeout(2130);
+            err2Ref.current!.style.display = 'block';
+
+            await timeout(2900);
+            err2Ref.current!.style.display = 'none';
+        };
+
+        showErrorMessages();
+    }, []);
 
     return (
         <div className={[styles.box, fontFamily.className].join(' ')}>
@@ -24,6 +53,12 @@ const Typed = () => {
                 backSpeed={60}
                 typeSpeed={10}
             />
+            <div style={{ display: 'none' }} ref={err1Ref} className={styles.errText}>
+                {err1}
+            </div>
+            <div style={{ display: 'none' }} ref={err2Ref} className={styles.errText}>
+                {err2}
+            </div>
         </div>
     );
 };
--- components/typed/useText.ts
+++ components/typed/useText.ts
@@ -3,7 +3,6 @@
 import { useEffect, useState } from 'react';
 
 import { generateString } from './helpers';
-import styles from './typed.module.scss';
 
 const useText = () => {
     const [strings, setStrings] = useState<string[]>(['']);
@@ -13,33 +12,7 @@ const useText = () => {
 
         setStrings(generateString());
 
-        const a = setTimeout(() => {
-            const err1 = document.createElement('span');
-            err1.classList.add(styles.errText);
-            err1.textContent = 'Type "number" is not assignable to type "string"';
-
-            const span = document.getElementById('err1Span');
-            span?.appendChild(err1);
-
-            if (span?.previousElementSibling?.classList?.add) {
-                span.previousElementSibling.classList.add(styles.bordered);
-            }
-
-            setTimeout(() => {
-                const err2 = document.createElement('span');
-                err2.classList.add(styles.errText);
-                err2.textContent = 'Type "string" is not assignable to type "string[]"';
-
-                const span = document.getElementById('err2Span');
-                span?.appendChild(err2);
-                if (span?.previousElementSibling?.classList?.add) {
-                    span.previousElementSibling.classList.add(styles.bordered);
-                }
-            }, 5100);
-        }, 3800);
-
         return () => {
-            clearTimeout(a);
             setStrings(['']);
         };
     }, []);
