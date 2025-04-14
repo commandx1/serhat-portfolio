cdc58d5b0545d81e7cc6c9138d9adac3af8d640f
commandx1 <serhatbelen7@gmail.com>
2025-04-14 16:08:02 +0300

    add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,26 @@
-/** @type {import('next-sitemap').IConfig} */
+const pages = [
+    '/',
+    '/about-me',
+];
+  
 module.exports = {
     siteUrl: 'https://serhatbelen.dev',
     generateRobotsTxt: true,
     generateIndexSitemap: true,
+    transform: async (_, path) => {
+        return {
+            loc: path,
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date().toISOString(),
+            alternateRefs: [],
+        };
+    },
+    additionalPaths: async () => {
+        return pages.map((page) => ({
+            loc: page,
+            lastmod: new Date().toISOString(),
+        }));
+    },
 };
+  
