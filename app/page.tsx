36c43bd085bb37b1604ec3ad9959fa23ac43c397
serhat-retrace <serhatbelen7@gmail.com>
2025-05-21 18:39:45 +0300

    Add AnimatedDiv with Typed component to enhance homepage layout.

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,16 @@ export default function Home() {
                 <Grid className={styles.banner} container spacing={8}>
                     <Grid size={{ xs: 12, lg: 6 }}>
                         <AnimatedDiv>
+                            <Typed />
+                        </AnimatedDiv>
+                    </Grid>
+                    <Grid size={{ xs: 12, lg: 6 }}>
+                        <AnimatedDiv>
                             <div className={styles.bannerContent}>
                                 <div className={[styles.dash, 'lineAnim'].join(' ')} />
                                 <div className={styles.desc}>{t('Hey')}</div>
                                 <div className={styles.subDesc}>{t('Desc')}</div>
                             </div>
-                        </AnimatedDiv>
-                    </Grid>
-                    <Grid size={{ xs: 12, lg: 6 }}>
-                        <AnimatedDiv>
-                            <Typed />
                         </AnimatedDiv>
                     </Grid>
                 </Grid>
